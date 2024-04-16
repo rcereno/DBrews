8fd9d8270e4589871cb70e98471c2749f1aa6a17
rcereno <rcereno@calpoly.edu>
2024-04-16 00:18:05 -0700

    updated barrels.py - v2 - p3

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -25,23 +25,49 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     with db.engine.begin() as connection:
         total_green_ml_val = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar_one()
         total_green_price_val = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
+        total_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar_one()
 
+        total_red_ml_val = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory")).scalar_one()
+        total_red_price_val = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
 
-        # total_red_ml_val = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory")).scalar_one()
-        # total_red_price_val = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
-
-        # total_blue_ml_val = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory")).scalar_one()
-        # total_blue_price_val = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
+        total_blue_ml_val = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory")).scalar_one()
+        total_blue_price_val = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
 
     for barrel in barrels_delivered:
-        amt_of_ml = barrel.ml_per_barrel * barrel.quantity
-        total_ml_val = total_ml_val + amt_of_ml
-        gold_price = barrel.quantity * barrel.price
-        total_price_val = total_price_val + gold_price
+        if barrel.price <= total_gold:
+            if barrel.sku == "SMALL_GREEN_BARREL":
+                amt_of_green_ml = barrel.ml_per_barrel * barrel.quantity
+                total_green_ml_val = total_green_ml_val + amt_of_green_ml
+                gold_price = barrel.quantity * barrel.price
+                total_green_price_val = total_green_price_val - gold_price
 
-        with db.engine.begin() as connection: 
-            set_ml = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = num_green_ml + {total_ml_val}"))
-            set_gold = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {total_price_val}"))
+                with db.engine.begin() as connection: 
+                    connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = num_green_ml + {total_green_ml_val}"))
+                    connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {total_green_price_val}"))
+
+            if barrel.sku == "SMALL_RED_BARREL":
+                amt_of_red_ml = barrel.ml_per_barrel * barrel.quantity
+                total_red_ml_val = total_red_ml_val + amt_of_red_ml
+                gold_price = barrel.quantity * barrel.price
+                total_red_price_val = total_red_price_val - gold_price
+
+                with db.engine.begin() as connection: 
+                    connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_ml = num_red_ml + {total_red_ml_val}"))
+                    connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {total_red_price_val}"))
+
+            if barrel.sku == "SMALL_BLUE_BARREL":
+                amt_of_blue_ml = barrel.ml_per_barrel * barrel.quantity
+                total_blue_ml_val = total_blue_ml_val + amt_of_blue_ml
+                gold_price = barrel.quantity * barrel.price
+                total_blue_price_val = total_blue_price_val - gold_price
+
+                with db.engine.begin() as connection: 
+                    connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_blue_ml = num_blue_ml + {total_blue_ml_val}"))
+                    connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {total_blue_price_val}"))
+
+        # with db.engine.begin() as connection: 
+        #     set_ml = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = num_green_ml + {total_green_ml_val}"))
+        #     set_gold = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {total_green_price_val}"))
 
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 

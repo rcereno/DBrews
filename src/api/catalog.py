1d1dc97dcb67cc5e77df410c4f7c1b8598254fca
rcereno <rcereno@calpoly.edu>
2024-04-22 13:45:31 -0700

    changed catalog.py to pv

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,39 +13,47 @@ def get_catalog():
 
     catalog = []
     with db.engine.begin() as connection:
-        green_quantity = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
-        red_quantity = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
-        blue_quantity = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
-        # scalar_one: changes it from the object to a number 
-        # do same process with other colors 
-        if green_quantity > 0:
-            catalog.append(
-                {
-                    "sku": "GREEN_POTION_0",
-                    "name": "green potion",
-                    "quantity": green_quantity,
-                    "price": 50,
-                    "potion_type": [0, 100, 0, 0],
-                })
+        results = connection.execute(sqlalchemy.text("SELECT inventory, sku, type, price from potion_inventory"))
+
+        for row in results:
+            catalog.append({"sku": row.sku, "quantity": row.inventory, "price": row.price, "potion_type": row.type})
+
+    return catalog 
+
+    # catalog = []
+    # with db.engine.begin() as connection:
+    #     green_quantity = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
+    #     red_quantity = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
+    #     blue_quantity = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
+    #     # scalar_one: changes it from the object to a number 
+    #     # do same process with other colors 
+    #     if green_quantity > 0:
+    #         catalog.append(
+    #             {
+    #                 "sku": "GREEN_POTION_0",
+    #                 "name": "green potion",
+    #                 "quantity": green_quantity,
+    #                 "price": 50,
+    #                 "potion_type": [0, 100, 0, 0],
+    #             })
         
-        if red_quantity > 0:
-            catalog.append(
-                {
-                    "sku": "RED_POTION_0",
-                    "name": "red potion",
-                    "quantity": red_quantity,
-                    "price": 50,
-                    "potion_type": [100, 0, 0, 0],
-                })
+    #     if red_quantity > 0:
+    #         catalog.append(
+    #             {
+    #                 "sku": "RED_POTION_0",
+    #                 "name": "red potion",
+    #                 "quantity": red_quantity,
+    #                 "price": 50,
+    #                 "potion_type": [100, 0, 0, 0],
+    #             })
             
-        if blue_quantity > 0:
-            catalog.append(
-                {
-                    "sku": "BLUE_POTION_0",
-                    "name": "blue potion",
-                    "quantity": blue_quantity,
-                    "price": 50,
-                    "potion_type": [0, 0, 100, 0],
-                })
-            
-    return catalog        
+    #     if blue_quantity > 0:
+    #         catalog.append(
+    #             {
+    #                 "sku": "BLUE_POTION_0",
+    #                 "name": "blue potion",
+    #                 "quantity": blue_quantity,
+    #                 "price": 50,
+    #                 "potion_type": [0, 0, 100, 0],
+    #             })
+    # return catalog        

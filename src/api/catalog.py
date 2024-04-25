f9e53aff94445c5c7ec3f968bc8ee2ca4cec86d9
rcereno <rcereno@calpoly.edu>
2024-04-24 22:40:22 -0700

    made some changes to catalog.py to pv's version

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -7,24 +7,27 @@ router = APIRouter()
 
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
+
+    # set up to just 6 potions?
+    
     """
     Each unique item combination must have only a single price.
     """
-
+    # pv:
     catalog = []
     with db.engine.begin() as connection:
-        results = connection.execute(sqlalchemy.text("SELECT inventory, sku, type, price from potion_inventory"))
-
+        # results = connection.execute(sqlalchemy.text("SELECT inventory, sku, type, price from potion_inventory"))
+        results = connection.execute(sqlalchemy.text("SELECT sku, price, quantity, potion_type from potion_inventory"))
         for row in results:
-            catalog.append({"sku": row.sku, "quantity": row.inventory, "price": row.price, "potion_type": row.type})
-
-    return catalog 
+            catalog.append({"sku": row.sku, "name": row.sku, "quantity": row.inventory, "price": row.potion_price, "potion_type": row.type})
+    return catalog
 
     # catalog = []
     # with db.engine.begin() as connection:
     #     green_quantity = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
     #     red_quantity = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
     #     blue_quantity = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
+    #     dark_quantity = connection.execute(sqlalchemy.text("SELECT num_dark_potions FROM global_inventory")).scalar_one()
     #     # scalar_one: changes it from the object to a number 
     #     # do same process with other colors 
     #     if green_quantity > 0:
@@ -55,5 +58,15 @@ def get_catalog():
     #                 "quantity": blue_quantity,
     #                 "price": 50,
     #                 "potion_type": [0, 0, 100, 0],
+    #             })
+            
+    #     if dark_quantity > 0:
+    #         catalog.append(
+    #             {
+    #                 "sku": "DARK_POTION_0",
+    #                 "name": "dark potion",
+    #                 "quantity": dark_quantity,
+    #                 "price": 50,
+    #                 "potion_type": [0, 0, 0, 100],
     #             })
     # return catalog        
